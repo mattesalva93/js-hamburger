2d582eb084a9a2a2f28fc6c23b31f224b280ebad
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-09 15:40:33 +0100

    show done

---
+++ main.js
@@ -0,0 +1,10 @@
+openmenu = document.querySelector(".fas.fa-bars");
+
+openmenu.addEventListener("click" , function(){
+    const showmenu = document.querySelector(".hamburger-menu");
+    showmenu.className = showmenu.classList + " active ";
+
+})
+
+
+
