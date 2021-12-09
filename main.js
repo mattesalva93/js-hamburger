fe3f612506a2454afacdd38474b238d9c46f98f3
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-09 16:06:27 +0100

    changing display style variant

--- main.js
+++ main.js
@@ -9,8 +9,20 @@ closemenu = document.querySelector(".fas.fa-times");
 
 closemenu.addEventListener("click" , function(){
     const hidemenu = document.querySelector(".hamburger-menu");
-    hidemenu.className = "hamburger-menu"
+    hidemenu.className = "hamburger-menu";
 })
 
 
+/*openmenu = document.querySelector(".fas.fa-bars");
 
+openmenu.addEventListener("click" , function(){
+    const showmenu = document.querySelector(".hamburger-menu");
+    showmenu.style.display = "block";
+})
+
+closemenu = document.querySelector(".fas.fa-times");
+
+closemenu.addEventListener("click" , function(){
+    const hidemenu = document.querySelector(".hamburger-menu");
+    hidemenu.style.display = "none";
+})*/
