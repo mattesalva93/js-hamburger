1b88a2dd99ad3f032b9db2656b921d150dab4d10
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-09 15:46:38 +0100

    hide menu done

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ openmenu = document.querySelector(".fas.fa-bars");
 openmenu.addEventListener("click" , function(){
     const showmenu = document.querySelector(".hamburger-menu");
     showmenu.className = showmenu.classList + " active ";
+})
 
+closemenu = document.querySelector(".fas.fa-times");
+
+closemenu.addEventListener("click" , function(){
+    const hidemenu = document.querySelector(".hamburger-menu");
+    hidemenu.className = "hamburger-menu"
 })
 
 
